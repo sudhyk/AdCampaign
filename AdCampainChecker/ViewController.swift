abac8751c791792fbe328127b4d966f9b2b46f70
Sudheendra Kaanugovi <nsstackdev@sudhy.com>
2017-01-12 20:04:29 -0500

    Initial Commit

---
+++ AdCampainChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdCampainChecker
+//
+//  Created by Sudheendra K Kaanugovi on 1/12/17.
+//  Copyright © 2017 Sudheendra K Kaanugovi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
